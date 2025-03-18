b78e630e7a012bd07b0b94e553fe53943ca02cb8
Lucas Mol <molzera2@gmail.com>
2025-03-18 10:29:51 -0300

    PRIMEIRO PROJETO BASICO

---
+++ createyourpassword777.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+if __name__ == "__main__":
+    tamanho = int(input("Digite o tamanho da senha: "))
+    print("Senha gerada:", gerar_senha(tamanho))
